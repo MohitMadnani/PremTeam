6cff85fb140d2075c023710ecfc72b6071f705ef
Mohit <mohitmadnani419@gmail.com>
2025-10-04 00:02:36 +0100

    More deploy updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',  // This tells Vite where to find index.html
   build: {
     outDir: 'build'
   },
